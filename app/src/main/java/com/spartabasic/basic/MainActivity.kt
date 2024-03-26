66f4d3b4916ab1559c0b63196e71a1f52ef193dd
yeonsoo940630 <wjddustn26@gmail.com>
2024-03-26 14:41:23 +0900

    과제 ìã피드백 수정

--- app/src/main/java/com/spartabasic/basic/MainActivity.kt
+++ app/src/main/java/com/spartabasic/basic/MainActivity.kt
@@ -23,9 +23,9 @@ import kotlin.random.Random
 class MainActivity : AppCompatActivity() {
     private var job: Job? = null
 
-    lateinit var button: Button
-    lateinit var randomTextView: TextView
-    lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var randomTextView: TextView
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,9 +52,6 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun setupButton() {
-        // TODO("activity_main.xml에 작성되어 있는 button을 findViewById를 사용하여 button이라는 Button 타입의 변수에 할당하기)
-
-        // 위 코드가 작성되어야 아래 코드가 수행될 수 있음!
         button.setOnClickListener {
             job?.cancel()
             checkAnswerAndShowToast()
@@ -62,30 +59,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setRandomValueBetweenOneToHundred() {
-        // TODO("activity_main.xml에 작성되어 있는 textViewRandom을 findViewById를 사용하여 randomTextView라는 TextView 타입의 변수에 할당하기)
-
-        // TODO("Kotlin Random을 찾아보고 1이상 100이하의 숫자를 랜덤으로 구하여 Int 타입의 randomValue라는 변수에 할당하는 코드 작성하기")
-
-        // TODO("위 random으로 구한 값을 randomTextView에 세팅하여 화면에 보여주도록 하기")
         val randomValue = Random.nextInt(100) + 1
         randomTextView.text = randomValue.toString()
     }
 
     private fun setJobAndLaunch() {
-        // TODO("activity_main.xml에 작성되어 있는 spartaTextView를 findViewById를 사용하여 textView라는 TextView 타입의 변수에 할당하기)
-
-        /*job = lifecycleScope.launch {
-            var i = 1
-            while (isActive && i <= 100) {
-                textView.text = i.toString()
-                delay(500)
-                i += 1 // ++i, i++
-            }
-        }*/
 
         job = lifecycleScope.launch {
-            // TODO("위 주석처리된 while 루프 코드를 참조하여 for 루프로 작성해보기(1이상 100이하)")
-            // 아래 /*...*/ 안에 코드를 작성하세요.
             for (i in 1..100) {
                 if (isActive) {
                     textView.text = i.toString()
@@ -97,15 +77,6 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun checkAnswerAndShowToast() {
-        // TODO("activity_main.xml에 작성되어 있는 spartaTextView를 findViewById를 사용하여 textView라는 TextView 타입의 변수에 할당하기)
-
-        // TODO("activity_main.xml에 작성되어 있는 textViewRandom을 findViewById를 사용하여 randomTextView라는 TextView 타입의 변수에 할당하기)
-
-        // TODO("if문 사용해보기 - 위에서 2개의 변수를 작성완료하고 나면 textView의 값과 randomTextView의 값이 같은지 다른지를 확인하여 Toast 띄우기")
-        /**
-         * Toast 사용 예)
-         * Toast.makeText(this, "메세지", Toast.LENGTH_SHORT).show() // Toast.LENGTH_SHORT 대신 Toast.LENGTH_LONG 또한 사용 가능
-         */
         val textView = textView.text.toString()
         val randomTextView = randomTextView.text.toString()
 
